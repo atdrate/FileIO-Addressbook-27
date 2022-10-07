c12c813293cad5454d59fa414a5d762f7568c4c4
subodhNagalwade <subodhnagalwade@gmail.com>
2022-10-07 15:23:10 +0530

    [SUBODH]UC-13 file IO adress book

---
+++ InputOutput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIoAddressBook
+{
+    public class InputOutput
+    {
+        public void Read(string path)
+        {
+            using (StreamReader sw = File.OpenText(path))
+            {
+                string line;
+                while ((line = sw.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+        }
+        public void Write(string path)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("FirstName   : Subodh");
+                sw.WriteLine("LastName    : Nagalwade");
+                sw.WriteLine("Address     : 12 ujjwal nagar");
+                sw.WriteLine("PhoneNumber : 1234567890");
+                sw.WriteLine("City        : Lakhni");
+                sw.WriteLine("State       : Maharastra ");
+                sw.WriteLine("Zip         : 441804");
+                sw.WriteLine("Email       : Sub@gmail.com");
+                sw.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FileIoAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InputOutput readOrWrite = new InputOutput();
+            string path = @"C:\Users\AZ\Desktop\myproject\FileIoAddressBook\AddressBookFile.txt";
+            readOrWrite.Read(path);
+            readOrWrite.Write(path);
+            Console.ReadLine();
+        }
+    }
+}
